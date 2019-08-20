83111b8a213678855bbe5fb5c96f0c060e72e372
renan <camposrenanrc@gmail.com>
2019-08-20 17:51:11 -0300

    adicionar exercicios

---
+++ enumerate.py
@@ -0,0 +1,6 @@
+def imprimir_qualquer_coisa (*args):
+
+    for numero in enumerate(args):
+        print(str(numero) + '.' + item)
+
+imprimir_qualquer_coisa('maçã', 'pastel', 'natação')
---
+++ exercicio-kwards.py
@@ -0,0 +1,10 @@
+# * ARGS -> tupla (não pode ser modificada)
+# *KWARDS -> dicionário (contem chave e valor (key e value) )
+
+# def imprimir_qualquer_coisa (numero, *args):
+#     for item in args:
+#         print(item)
+#
+# imprimir_qualquer_coisa('0', 'maçã')
+# imprimir_qualquer_coisa('1', 'pastel')
+# imprimir_qualquer_coisa('2','natação')
